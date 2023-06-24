3d31cc5795fbdc9dbae838ea3d210acf142c93b5
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2023-06-24 19:05:22 +0800

    first commit

---
+++ src/Hours_Location.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hours_Location() {
+  return (
+    <div>Hours_Location</div>
+  )
+}
+
+export default Hours_Location;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './Home.jsx'
+import Menu from './Menu.jsx'
+import About from './About.jsx';
+import Hours_Location from "./Hours_Location.jsx"
+import FAQ from './FAQ.jsx'
+import ContactUs from './ContactUs.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <Home />,
+    // <About />,
+    // <Menu />,
+    // <Hours_Location />,
+    // <ContactUs />,
+    <FAQ />
+)
