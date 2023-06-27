459834d6f1f803a6b58feed0fe21fcfb456e5d0d
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2023-06-27 20:12:40 +0800

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import FAQ from './pages/FAQ'
-import Hours_Location from './pages/Hours_Location'
 
 function App() {
   return (
-    // <FAQ/>,
-    <Hours_Location/>
+    <FAQ/>
   )
 }
 
