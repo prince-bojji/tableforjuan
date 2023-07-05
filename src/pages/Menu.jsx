6b0aea9b28e82a909ae597c87b2f2e3e939a5758
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2023-07-05 08:24:55 +0800

    hatdog

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,72 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import Footer from '../components/Footer'
-import Header from '../components/Header'
->>>>>>> 5670bc52f953b6774a063309e5aaa94245e92e89
 
 function Menu() {
   return (
     <>
-<<<<<<< HEAD
-=======
-      <Header />
->>>>>>> 5670bc52f953b6774a063309e5aaa94245e92e89
-      <div id="MENU">
-        <h1 id="section">MENU</h1>
-        <style>
-          {`
-            h1 {
-              text-align: center;
-              color: #E79898;
-            }
-          `}
-        </style>
-        <div className="image-grid">
-          <div className="image-grid-item reveal">
-            <img src="/src/images/Menu/MENU_FRIEDBANGUS1.png" alt="Fried Bangus" />
-            <div className="image-caption">FRIED BANGUS</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_MUNGGOSTEW1.png" alt="Munggo Stew" />
-            <div className="image-caption">MUNGGO STEW</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_NILAGA1.png" alt="Nilagang Baka" />
-            <div className="image-caption">NILAGANG BAKA</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_PORKHUMBA1.png" alt="Pork Humba" />
-            <div className="image-caption">PORK HUMBA</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_BAKEDBANGUS1.png" alt="Baked Bangus" />
-            <div className="image-caption">BAKED BANGUS</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_MENUDO1.png" alt="Menudo" />
-            <div className="image-caption">MENUDO</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_BEEFBROCCOLI1.png" alt="Beef Broccoli" />
-            <div className="image-caption">BEEF BROCCOLI</div>
-          </div>
-          <div className="image-grid-item reveal">
-            <img src="/images/Menu/MENU_SAYOTE1.png" alt="Sayote" />
-            <div className="image-caption">SAYOTE</div>
-          </div>
-          <div className="image-grid-item reveal" id="beef">
-            <img src="/images/Menu/MENU_BEEFINMUSHROOMSAUCE1.png" alt="Beef in Mushroom Sauce" />
-            <div className="image-caption">BEEF IN MUSHROOM SAUCE</div>
-          </div>
-        </div>
-      </div>
-<<<<<<< HEAD
-=======
-      <Footer />
->>>>>>> 5670bc52f953b6774a063309e5aaa94245e92e89
+      
     </>
-  );
+  )
 }
 
 export default Menu;
