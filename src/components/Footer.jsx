741332a8e048ca8b5a7a2ffd8bc4e8b5f9dc93f3
QueencessMay <munsayac.queencessmay@gmail.com>
2023-07-18 21:46:06 +0800

    Fixed Collapsible animation issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../images/Home/footer_logo.png';
 import fb from '../images/Home/footer_facebook.png';
@@ -6,21 +6,41 @@ import insta from '../images/Home/footer_insta.png';
 
 const Collapsible = (props) => {
   const [isOpen, setOpen] = useState(false);
+  const [isLargeScreen, setLargeScreen] = useState(false);
+  const [height, setHeight] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setLargeScreen(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setHeight((isOpen || isLargeScreen) ? ref.current?.getBoundingClientRect().height : 0);
+  }, [isOpen, isLargeScreen]);
 
   return (
     <div>
       <div className='flex justify-between items-center pb-2'>
         <h1 className='font-semibold text-sm'>{props.title}</h1>
-        <button className={`flex text-3xl lg:hidden transition duration-700 ${isOpen && 'rotate-180'}`} onClick={() => setOpen(!isOpen)}>
+        <button className={`flex text-3xl lg:hidden transition duration-500 ${isOpen && 'rotate-180'}`} onClick={() => setOpen(!isOpen)}>
           <ion-icon name='caret-down-circle'></ion-icon>
         </button>
       </div>
-      <div className={`max-h-0 lg:max-h-fit overflow-hidden transition-all duration-700 ${isOpen && 'max-h-fit mb-5'}`}>
-        {props.children}
+      <div className={`overflow-hidden transition-all duration-500 ${isOpen && 'mb-5'}`} style={{height}}>
+        <div ref={ref}>{props.children}</div>
       </div>
     </div>
   )
-}
+};
 
 const ImageLink = (props) => {
   return (
@@ -28,11 +48,11 @@ const ImageLink = (props) => {
       <img src={props.image} alt={props.name} className='w-10 h-auto' />
     </a>
   )
-}
+};
 
 const Footer = () => {
   const navigate = useNavigate();
-  
+
   return (
     <footer className='flex flex-col lg:flex-row justify-evenly py-12 px-5 bg-[#DEC1B9] font-montserrat text-[#9E6B67] text-[0.725rem]'>
       <div className='px-20 m-auto lg:m-0 w-fit text-center order-1 lg:-order-1'>
@@ -61,6 +81,6 @@ const Footer = () => {
       </Collapsible>
     </footer>
   )
-}
+};
 
-export default Footer
+export default Footer;
