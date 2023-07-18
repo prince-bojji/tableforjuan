b50206fb7e247cb986641d92165cbd1a60492b86
QueencessMay <munsayac.queencessmay@gmail.com>
2023-07-18 13:24:31 +0800

    Fixed layout issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import banner from '../images/Home/banner.png';
 import friedBangus from '../images/Home/friedbangus.png';
 import bakedBangus from '../images/Home/bakedbangus.jpg';
 import beefBrocoli from '../images/Home/beef_with_brocoli.jpg';
@@ -18,17 +17,17 @@ const Home = () => {
   return (
     <div className='bg-[#fff7f4] font-montserrat'>
 
-      <img src={banner} className='w-screen h-screen object-cover' />
-      <div className='flex justify-center items-center w-screen h-[40vh] bg-cover bg-[url("/src/images/Home/wood.png")]'>
+      <div className='w-full h-screen bg-fixed bg-center bg-cover bg-[url("/src/images/Home/banner.png")]' />
+      <div className='flex justify-center items-center w-full h-[40vh] bg-cover bg-[url("/src/images/Home/wood.png")]'>
         <p className='px-5 font-courgette text-[#9E6B67] text-center text-3xl md:text-5xl'>
           A Family's Passion for Food and Community <br />- Table for Juan
         </p>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 w-[90vw] lg:w-[70vw] h-[60vh] py-10 m-auto overflow-hidden'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 items-center w-[90vw] lg:w-[70vw] h-[60vh] py-10 m-auto overflow-hidden'>
         <img
-          src={friedBangus}
-          className='hidden lg:block w-full h-full object-cover'
-        />
+              src={friedBangus}
+              className='hidden lg:block w-full h-full object-cover overflow-hidden'
+          />
         <div className='flex flex-col justify-center font-semibold text-2xl'>
           <p className='text-[#71A78C]'>
             Kumusta? Table for Juan represents a carinderia for the Juans(one)
@@ -40,7 +39,7 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center w-screen lg:h-screen px-6 py-10 bg-cover bg-[url("/src/images/Home/best_seller_bg.png")]'>
+      <div className='flex flex-col justify-center items-center w-full lg:h-screen px-6 py-10 bg-fixed bg-cover bg-[url("/src/images/Home/best_seller_bg.png")]'>
         <h1 className='font-extrabold text-[#9E6B67] text-5xl'>Best Sellers</h1>
         <div className='flex flex-col lg:flex-row gap-5 mt-10 mb-3'>
           <BestSeller image={bakedBangus} />
